11c9a2e5c933575892e8acda7eb2e2d52ea22b84
RasmeiPagna <rasmeipagna.toung@gmail.com>
2022-05-18 14:26:53 +0200

    create variable

--- ocr.py
+++ ocr.py
@@ -1,6 +1,15 @@
 def main ():
-    print("Hello Pagna !")
-    print("Coucou tout le monde")
+    #creation d'une variable 'usename' ayant pour valeur le mot 'Pagna'
+    username = "Pagna"
+    #creation d'une variable 'age' ayant pour valeur '32'
+    age = 32
+    #le porte monnaie
+    wallet = 125.7
+    #booléen true/false
+    is_happy = True
+    # print("Hello Pagna !")
+    # print("Coucou tout le monde")
+
 
 if __name__=='__main__':
     main()
