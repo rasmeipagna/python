87049f4f2ec490c3cea9a3ff9d886d7225e44336
RasmeiPagna <rasmeipagna.toung@gmail.com>
2022-05-18 14:38:32 +0200

    update calcul moyenne

--- helloworld.py
+++ helloworld.py
@@ -1,14 +1,21 @@
 def main ():
     #creation d'une variable 'usename' ayant pour valeur le mot 'Pagna'
     username = "Pagna"
+    username = "Youtube"
     #creation d'une variable 'age' ayant pour valeur '32'
     age = 32
     #le porte monnaie
     wallet = 125.7
     #booléen true/false
     is_happy = True
-    # print("Hello Pagna !")
-    # print("Coucou tout le monde")
+    #affiche la valeur du username et l'age
+    print(username, age)
+    #change la valeur de age de 32 à 25
+    age = 25
+    print(age)
+    age = age + 1
+    print(age)
+    print("Salut " + username + ", vous avez " + str(age) + " ans !")
 
 
 if __name__=='__main__':
--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-if __name__== '_main':
-    print("Hello world !")
+def main ():
+    #recolter une premiere note
+    note1 = int(input("Entrer la premiere note"))
+    #recolter la deuxieme note
+    note2 = int(input("Entrer la seconde note"))
+    #recolter la derniere note
+    note3 = int(input("Entrer la derniere note"))
+    #calculer la moyenne
+    result = (note1 + note2 + note3)/ 3
+    #afficher le resultat
+    print("La moyenne de l'eleve est de " + str(result))
+
+
+if __name__=='__main__':
+    main()
