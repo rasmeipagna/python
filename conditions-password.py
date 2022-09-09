30936c232b44f96ed766c584920038eeb90fcf83
RasmeiPagna <rasmeipagna.toung@gmail.com>
2022-09-09 09:48:44 +0200

    conditions password

--- conditions-password.py
+++ conditions-password.py
@@ -5,7 +5,9 @@ password_length = len(password)
 #vérifier si le mot de passe est inferieur a 8 caracteres
 
 if password_length <= 8:
-    print("Mot de passe trop court !password_length")
+    print("Mot de passe trop court !")
+elif 8 < password_length <= 12:
+    print("Mot de passe moyen")
 else:
     print("Mot de passe valide")
 print(password_length)
