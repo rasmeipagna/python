b2d849640ce060cfdad9ee0127003ae4330be5ad
RasmeiPagna <rasmeipagna.toung@gmail.com>
2022-09-15 10:49:40 +0200

    listes ajout et supprime noms

--- listes.py
+++ listes.py
@@ -11,3 +11,22 @@ online_players[0] = "Rasmeipagna"
 
 online_players[2:4] = ["Paul", "Jacques"]
 print(online_players[2:4])
+
+#en ajouter d'autres
+online_players.append("Gamer123")
+online_players.extend(["Gogum1er"])
+
+#virer un joueur
+del online_players[1]
+#ou
+online_players.remove("Paul")
+#ou
+online_players.clear("Paul")
+
+
+print(online_players)
+
+#exemple : jouer à la maitresse
+
+notes = [8, 12, 10, 9, 4, 20]
+print(notes)
