d33e9a5780c17098c8424e118a31c4799b1ec54a
RasmeiPagna <rasmeipagna.toung@gmail.com>
2022-09-15 10:27:19 +0200

    injecter en remplacer une valeur par une autre

--- listes.py
+++ listes.py
@@ -1,3 +1,13 @@
 #créer une liste ui va stocker des pseudos pour simuler un jeu en ligne
-online_players = ["Pagna","Anana","Cleymax"]
+online_players = ["Pagna","Anana","Cleymax","Bob"]
 print(online_players)
+print(online_players[0])
+print(online_players[2])
+#ou
+print(online_players[len(online_players) - 1])
+
+#modifier 'Pagna' ->'Rasmeipagna'
+online_players[0] = "Rasmeipagna"
+
+online_players[2:4] = ["Paul", "Jacques"]
+print(online_players[2:4])
