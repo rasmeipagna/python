d1d531086f9661e9ee7b8c2d391c786fb03d6a01
RasmeiPagna <rasmeipagna.toung@gmail.com>
2022-09-15 11:29:52 +0200

    update liste moyenne

--- listes.py
+++ listes.py
@@ -1,32 +1,20 @@
+from statistics import mean
 #créer une liste ui va stocker des pseudos pour simuler un jeu en ligne
 online_players = ["Pagna","Anana","Cleymax","Bob"]
-print(online_players)
-print(online_players[0])
-print(online_players[2])
-#ou
-print(online_players[len(online_players) - 1])
 
-#modifier 'Pagna' ->'Rasmeipagna'
-online_players[0] = "Rasmeipagna"
-
-online_players[2:4] = ["Paul", "Jacques"]
-print(online_players[2:4])
-
-#en ajouter d'autres
-online_players.append("Gamer123")
-online_players.extend(["Gogum1er"])
-
-#virer un joueur
-del online_players[1]
-#ou
-online_players.remove("Paul")
-#ou
-online_players.clear("Paul")
-
+#que le joueur Anana de deconnecte
+online_players.remove("Bob")
+#supprimer tous les elements de votre liste
+online_players.clear()
 
 print(online_players)
+
 
 #exemple : jouer à la maitresse
 
-notes = [8, 12, 10, 9, 4, 20]
+notes = [8, 12, 10, 9, 4, 20, 14, 3]
 print(notes)
+
+#module : statistics
+result = mean(notes)
+print("La moyenne de l'eleve est {".format(result))
